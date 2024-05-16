c4a4b54b165464b8f51e881846de50d5bf0106d5
Nodari Muselishvili <85808236+sp33k1337@users.noreply.github.com>
2024-05-16 23:36:38 +0400

    omg

---
+++ src/js/main.js
@@ -0,0 +1,14 @@
+const billInput = document.getElementById("bill-input");
+const tipBtn = document.getElementById("tip-btn");
+const tipsButtons = document.getElementById("tips");
+const customTip = document.getElementById("custom-tip");
+const quantity = document.getElementById("quantity");
+const personSum = document.getElementById("person-sum");
+const totalSum = document.getElementById("total-sum");
+const resetBtn = document.getElementById('reset-btn');
+
+let billValue = 0;
+let tipsValue = 0;
+let person = 0;
+let total = 0;
+let tipResult = 0;
---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  // purge: [
+  //   './src/**/*.html',
+  //   './src/**/*.js',
+  // ],
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        'space-mono': ["Space Mono", 'monospace'],
+      }
+    },
+  },
+  plugins: [],
+}
